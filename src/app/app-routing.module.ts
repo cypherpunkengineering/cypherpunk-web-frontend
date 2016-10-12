813240b20616f04842388807e8429faa44d17c55
Edward Kim <ed@slickage.com>
2016-10-11 15:23:14 -1000

    Public navigation, footer, and home page
    The ng2 app was bootstrapped with a simple routing system and the first
    page (home page) along with the public navigation and footer were
    created. The folder structure for the project has been laid out with
    more content to be created in the future.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './layouts/public/home/home.component';
+import { NotFoundComponent } from './layouts/404/404.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, data: { title: 'home' } },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
   providers: []
 })
-export class DesktopRoutingModule { }
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
 
-import { AppComponent } from './app.component';
+import { RootComponent } from './app-root.component';
+import { HomeComponent } from './layouts/public/home/home.component';
+import { NotFoundComponent } from './layouts/404/404.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
+  // all components must be declared here
   declarations: [
-    AppComponent
+    RootComponent,
+    HomeComponent,
+    NotFoundComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RootComponent]
 })
 export class AppModule { }
