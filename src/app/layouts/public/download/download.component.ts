3041612c71985bde483325282b9fd047876c45a8
Edward Kim <ed@slickage.com>
2016-10-16 21:02:05 -1000

    Updated navigation to match latest mockups

--- src/app/layouts/public/download/download.component.ts
+++ src/app/layouts/public/download/download.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavigationEnd } from '@angular/router';
 
 @Component({
   templateUrl: './download.component.html',
   styleUrls: ['./download.component.scss']
 })
-export class DownloadComponent { }
+export class DownloadComponent {
+  constructor(router: Router) {
+    router.events.subscribe(s => {
+      if (s instanceof NavigationEnd) {
+        const tree = router.parseUrl(router.url);
+        if (tree.fragment) {
+          // you can use DomAdapter
+          const element = document.querySelector('#' + tree.fragment);
+          if (element) {
+            element.scrollIntoView({
+             behavior: 'smooth'
+            });
+          }
+        }
+      }
+    });
+  }
+}
