facb63d542a5c4f097a61ee4cf7fdc2d7cb5e378
Edward Kim <ed@slickage.com>
2016-10-25 16:48:15 -1000

    Initial implementation for a login service

--- src/app/layouts/public/login/login.component.ts
+++ src/app/layouts/public/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
+import { AlertService } from '../../../services/alert.service';
 
 @Component({
   templateUrl: './login.component.html',
@@ -8,16 +9,21 @@ import { AuthService } from '../../../services/auth.service';
 })
 export class LoginComponent {
   user = {
-    email: '',
+    login: '',
     password: ''
   };
 
-  constructor(private router: Router, private auth: AuthService) {}
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+    private alertService: AlertService
+  ) {}
 
   login() {
+    let alertService = this.alertService;
+
     this.auth.login(this.user)
-    .then((val) => {
-      this.router.navigate(['user']);
-    });
+    .then(() => { this.router.navigate(['user']); })
+    .catch(function() { alertService.error('Could not log in'); });
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,38 @@
 import { Injectable } from '@angular/core';
 import { SessionService } from './session.service';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
 
 @Injectable()
 export class AuthService {
   authed: boolean = false;
   redirectUrl: string;
 
-  constructor(private session: SessionService) {}
+  constructor(
+    private session: SessionService,
+    private http: Http
+  ) {}
 
   login(user) {
-    this.authed = true;
-    return Promise.resolve(this.authed);
+    let session = this.session;
+    let url = '/account/authenticate/userpasswd';
+    let body = { login: user.login, password: user.password};
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({ headers: headers });
+    return this.http.post(url, body, options).toPromise()
+    .then(function(res: Response) {
+      console.log(res);
+      return res.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      session.user.email = data.acct.email;
+      session.user.secret = data.secret;
+      this.authed = true;
+    });
   }
 
-  logout() { this.authed = false; }
+  logout() {
+    // clear session data and secret
+    this.authed = false;
+  }
 }
