bce2d4e6d61d47757bdf83f0ced49e9fd9a8293a
Edward Kim <ed@slickage.com>
2016-11-02 16:36:27 -1000

    Scroll to top of page on page change

--- src/app/app-root.component.ts
+++ src/app/app-root.component.ts
@@ -1,7 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>'
 })
-export class RootComponent { }
+export class RootComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) { return; }
+
+      const tree = this.router.parseUrl(this.router.url);
+      if (tree.fragment) {
+        // you can use DomAdapter
+        const element = document.querySelector('#' + tree.fragment);
+        if (element) {
+          element.scrollIntoView({
+           behavior: 'smooth'
+          });
+        }
+      }
+      else { document.body.scrollTop = 0; }
+    });
+  }
+}
--- src/app/layouts/public/download/download.component.ts
+++ src/app/layouts/public/download/download.component.ts
@@ -1,26 +1,7 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { NavigationEnd } from '@angular/router';
 
 @Component({
   templateUrl: './download.component.html',
   styleUrls: ['./download.component.scss']
 })
-export class DownloadComponent {
-  constructor(router: Router) {
-    router.events.subscribe(s => {
-      if (s instanceof NavigationEnd) {
-        const tree = router.parseUrl(router.url);
-        if (tree.fragment) {
-          // you can use DomAdapter
-          const element = document.querySelector('#' + tree.fragment);
-          if (element) {
-            element.scrollIntoView({
-             behavior: 'smooth'
-            });
-          }
-        }
-      }
-    });
-  }
-}
+export class DownloadComponent { }
