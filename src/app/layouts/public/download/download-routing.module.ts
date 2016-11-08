ef21667ed898d7e2d4a80298cdd7ad7a4755fb64
Edward Kim <ed@slickage.com>
2016-11-07 22:10:08 -1000

    Testing route redirect for html pages

--- src/app/layouts/public/download/download-routing.module.ts
+++ src/app/layouts/public/download/download-routing.module.ts
@@ -6,7 +6,8 @@ import { DownloadComponent } from './download.component';
 @NgModule({
   imports: [
     RouterModule.forChild([
-      { path: 'download', component: DownloadComponent }
+      { path: 'download', component: DownloadComponent },
+      { path: 'download.html', redirectTo: '/download' }
     ])
   ]
 })
