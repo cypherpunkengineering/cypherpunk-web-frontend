6cf27254e7495a99a6e3a7d957a56a2343b3ccb6
J. Maurice <j@wiz.biz>
2016-11-22 17:40:07 +0900

    Re-install global angular-cli dep from jenkins build script

--- build.sh
+++ build.sh
@@ -1,15 +1,36 @@
 #!/bin/bash -x
 set -e
+
+# prepare node/npm
 source $HOME/.nvm/nvm.sh v7.1.0
 nvm install v7.1.0
 nvm use v7.1.0
+
+# prepare global deps
+npm uninstall -g angular-cli
+npm cache clean
+npm install -g node-gyp
+npm install -g node-pre-gyp
+npm install -g angular-cli
+
+# prepare local deps
 npm install
+
+# start build
 npm start &
 npm run build:prod
 sleep 5
+
+# start scrape
 npm run scrape
+
+# deploy to appengine
 cd appengine
 mvn install
 mvn appengine:update || true
+
+# kill everything
 pkill -P $$
+
+# done
 exit 0
