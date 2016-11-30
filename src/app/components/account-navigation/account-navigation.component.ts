e557d21d9375307465b9658fa3bb17bdcab557a6
Edward Kim <ed@slickage.com>
2016-11-29 14:29:07 -1000

    Scrolled navigation on /account pages

--- src/app/components/account-navigation/account-navigation.component.ts
+++ src/app/components/account-navigation/account-navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 
@@ -9,11 +9,30 @@ import { Router } from '@angular/router';
 })
 export class AccountNavigationComponent {
   showDropDown = false;
+  scrolledNavElement: HTMLElement;
 
   constructor(private router: Router, private auth: AuthService) { }
 
   logout() {
     this.auth.logout()
     .then(() => { this.router.navigate(['/']); });
+  }
+
+  // on scroll,
+  @HostListener('window:scroll', ['$event'])
+  handleScrollEvent(event) {
+    this.scrolledNavElement = document.getElementById('scrolled-nav');
+
+    // we round here to reduce a little workload
+    let currentPosition = Math.round(window.scrollY);
+
+    if (currentPosition > 60 && this.scrolledNavElement) {
+      this.scrolledNavElement.style.opacity = '1';
+      this.scrolledNavElement.style.visibility = 'visible';
+    }
+    else {
+      this.scrolledNavElement.style.opacity = '0';
+      this.scrolledNavElement.style.visibility = 'hidden';
+    }
   }
 }
