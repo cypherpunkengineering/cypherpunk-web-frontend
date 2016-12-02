dc9fd74567bede74c6bbaa4ec291664da817b273
Edward Kim <ed@slickage.com>
2016-12-01 15:04:03 -1000

    Updated wording on /download and /home

--- src/app/layouts/public/home/home.component.ts
+++ src/app/layouts/public/home/home.component.ts
@@ -1,9 +1,8 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "home",
-  templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.scss"]
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
 
