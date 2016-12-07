1e4ff390557de6fc171856b652e56e328fc55e38
Edward Kim <ed@slickage.com>
2016-12-07 10:55:21 -1000

    Updated SEO
     * update home page headers and image attributes
     * update howitworks headers and image attributes
     * update whyus headers and image attributes
     * update header and footer image attribues

--- src/app/layouts/public/howitworks/howitworks.component.ts
+++ src/app/layouts/public/howitworks/howitworks.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   templateUrl: './howitworks.component.html',
   styleUrls: ['./howitworks.component.scss']
 })
-export class HowitworksComponent { }
+export class HowitworksComponent implements AfterViewInit {
+  title: string = 'How Cypherpunk Privacy Protects Your Online Privacy and Freedom';
+  description: string = 'Learn how Cypherpunk Privacy provides unrestricted access to the internet and protects your privacy online.';
+
+  constructor(@Inject(DOCUMENT) private document: any) { }
+
+  ngAfterViewInit(): void { this.document.title = this.title; }
+}
--- src/app/layouts/public/whyus/whyus.component.ts
+++ src/app/layouts/public/whyus/whyus.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   templateUrl: './whyus.component.html',
   styleUrls: ['./whyus.component.scss']
 })
-export class WhyusComponent { }
+export class WhyusComponent implements AfterViewInit {
+  title: string = 'Why You Need Cypherpunk Privacy';
+  description: string = 'Cypherpunk Privacy provides unrestricted access to the internet, protects your privacy online and secures public WiFi networks.';
+
+  constructor(@Inject(DOCUMENT) private document: any) { }
+
+  ngAfterViewInit(): void { this.document.title = this.title; }
+}
