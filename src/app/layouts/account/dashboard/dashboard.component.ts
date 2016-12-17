c1fb48e688386104f0a62e8359f99774b2f577a5
Edward Kim <ed@slickage.com>
2016-12-16 15:10:00 -1000

    Reverted some changes to the account page

--- src/app/layouts/account/dashboard/dashboard.component.ts
+++ src/app/layouts/account/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthGuard } from '../../../services/auth-guard.service';
 import { SessionService } from '../../../services/session.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { PlansService } from '../../../services/plans.service';
 
 @Component({
   templateUrl: './dashboard.component.html',
@@ -9,15 +10,20 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class DashboardComponent {
   user: any;
-  currentTab: string = 'overview';
   loading: boolean = true;
   showEmailModal: boolean = false;
   showPasswordModal: boolean = false;
+
+  // payment plans
+  plans = this.plansService.plans;
+  selectPlan = this.plansService.selectPlan;
+  selectedPlan = this.plansService.selectedPlan;
 
   constructor(
     private router: Router,
     private authGuard: AuthGuard,
     private session: SessionService,
+    private plansService: PlansService,
     private activatedRoute: ActivatedRoute
   ) {
     this.user = this.session.user;
@@ -28,17 +34,17 @@ export class DashboardComponent {
     .then((data) => { this.loading = data.loading || false; });
   }
 
-  openEmailModal() {
-    this.showEmailModal = true;
-    setTimeout(() => { document.getElementById('dashboardEmail').focus(); }, 510);
+  hidePriceBoxes() {
+    let type = this.user.account.type;
+    let renewal = this.user.subscription.renewal;
+
+    if (type === 'free') { return false; }
+    else if (type === 'premium' && renewal !== 'annually') { return false; }
+    return true;
   }
 
-  openPasswordModal() {
-    this.showPasswordModal = true;
-    setTimeout(() => { document.getElementById('dashboardPassword').focus(); }, 510);
-  }
-
-  switch(tab: string) {
-    this.currentTab = tab;
+  upgrade(id) {
+    this.selectPlan(id);
+    this.router.navigate(['/account/upgrade']);
   }
 }
