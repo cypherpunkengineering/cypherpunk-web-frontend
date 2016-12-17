230fbd4c82419f1c685e9a4a6b3e5d4617800f5a
Edward Kim <ed@slickage.com>
2016-12-16 16:59:18 -1000

    Download countdown and model on /download

--- src/app/layouts/public/download/download.component.ts
+++ src/app/layouts/public/download/download.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import * as platform from 'platform';
+import { isBrowser } from 'angular2-universal';
 
 @Component({
   templateUrl: './download.component.html',
@@ -10,12 +11,12 @@ export class DownloadComponent {
     mac: {
       name: 'Mac',
       versions: '',
-      link: ''
+      link: '#'
     },
     windows: {
       name: 'Windows',
       versions: '',
-      link: ''
+      link: '#'
     },
     linux: {
       name: 'Linux',
@@ -77,22 +78,82 @@ export class DownloadComponent {
   headerBuild = this.builds.blank;
   currentLinuxVersion: string = this.linuxVersions[0].link;
 
-  constructor() {
-    let os: string = platform.os.family;
+  // download
+  thanks: string = '';
+  countdown: number = 3;
+  downloadBuildName: string = '';
+  showDownloadModal: boolean = false;
+  isChrome: boolean = false;
+  isSafari: boolean = false;
 
-    if (os.indexOf('OS X') > -1) {
-      this.headerBuild = this.builds.mac;
-    }
-    else if (os.indexOf('Window') > -1) {
-      this.headerBuild = this.builds.windows;
-    }
-    else if (os.indexOf('Android') > -1) {
-      this.headerBuild = this.builds.android;
-    }
-    else if (os.indexOf('iOS') > -1) {
-      this.headerBuild = this.builds.ios;
-    }
+  constructor() {
+    // detect os setup
+    let os: string = platform.os.family;
+    if (os.indexOf('OS X') > -1) { this.headerBuild = this.builds.mac; }
+    else if (os.indexOf('Window') > -1) { this.headerBuild = this.builds.windows; }
+    else if (os.indexOf('Android') > -1) { this.headerBuild = this.builds.android; }
+    else if (os.indexOf('iOS') > -1) { this.headerBuild = this.builds.ios; }
     else { this.headerBuild = this.builds.linux; }
+
+    // download file setup
+    if (isBrowser) {
+      this.isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+      this.isSafari = navigator.userAgent.toLowerCase().indexOf('safari') > -1;
+    }
   }
+
+  launchDownloadModal(build: any): void {
+    this.thanks = '';
+    this.countdown = 3;
+    this.downloadBuildName = build.name;
+    this.showDownloadModal = true;
+
+    // figure out correct link
+    let link = build.link;
+    if (build.name === 'Linux') { link = this.currentLinuxVersion; }
+
+    // countdown timer
+    let intervalId = setInterval(() => {
+      this.countdown = this.countdown - 1;
+      if (this.countdown === 0) {
+        clearInterval(intervalId);
+        this.thanks = 'Thank you for downloading Cypherpunk Privacy.';
+      }
+    }, 1000);
+
+    // download file
+    setTimeout(() => {
+      this.downloadFile(link);
+    }, 3100);
+  }
+
+  downloadFile (sUrl: string): boolean {
+    // If in Chrome or Safari - download via virtual link click
+    if (this.isChrome || this.isSafari) {
+      // Creating new link node.
+      let link = document.createElement('a');
+      link.href = sUrl;
+
+      if (link.download !== undefined) {
+        // Set HTML5 download attribute. This will prevent file from opening if supported.
+        let fileName = sUrl.substring(sUrl.lastIndexOf('/') + 1, sUrl.length);
+        link.download = fileName;
+      }
+
+      // Dispatching click event.
+      if (document.createEvent) {
+        let e = document.createEvent('MouseEvents');
+        e.initEvent('click', true, true);
+        link.dispatchEvent(e);
+        return true;
+      }
+    }
+
+    // Force file download (whether supported by server).
+    if (sUrl.indexOf('?') === -1) { sUrl += '?download'; }
+
+    window.open(sUrl, '_self');
+    return true;
+  };
 
 }
