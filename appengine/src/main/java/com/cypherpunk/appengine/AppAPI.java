075c30a7b5a69b847e291c034793b77432c58659
J. Maurice <j@wiz.biz>
2017-01-04 19:22:38 +0900

    Fix typo in method name

--- appengine/src/main/java/com/cypherpunk/appengine/AppAPI.java
+++ appengine/src/main/java/com/cypherpunk/appengine/AppAPI.java
@@ -58,7 +58,7 @@ public class AppAPI extends HttpServlet
 
 		switch (apiMethod) {
 
-			case "status": { // {{{
+			case "versions": { // {{{
 				out.println("{\"windows\":{\"latest\":\"0.4.0-beta\",\"required\":\"0.3.0\",\"description\":\"A new version is available, please update your Cypherpunk Privacy app from https://cypherpunk.com/download\"},\"macos\":{\"latest\":\"0.4.0-beta\",\"required\":\"0.3.0\",\"description\":\"A new version is available, please update your Cypherpunk Privacy app from https://cypherpunk.com/download\"},\"linux\":{\"latest\":\"0.4.0-beta\",\"required\":\"0.3.0\",\"description\":\"A new version is available, please update your Cypherpunk Privacy app from https://cypherpunk.com/download\"},\"android\":{\"latest\":56,\"required\":55,\"description\":\"A new version is available, please update your Cypherpunk Privacy app from Google Play.\"},\"ios\":{\"latest\":56,\"required\":55,\"description\":\"A new version is available, please update your Cypherpunk Privacy app from the iTunes App store.\"},\"chrome\":{\"latest\":56,\"required\":55,\"description\":\"A new version is available, please update your Cypherpunk Privacy app from the Chrome webstore.\"},\"firefox\":{\"latest\":56,\"required\":55,\"description\":\"A new version is available, please update your Cypherpunk Privacy app from the Mozilla Add-ons.\"}}");
 			}
 			break; // }}}
