ddcf47417323fe41ba39a87757a7269b6f457fe4
Edward Kim <ed@slickage.com>
2017-01-15 18:31:13 +0900

    Fix Back button bug on /apps

--- src/app/layouts/public/apps/apps/apps.component.ts
+++ src/app/layouts/public/apps/apps/apps.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import * as platform from 'platform';
+import { Location } from '@angular/common';
 
 @Component({ template: '' })
 export class AppsComponent {
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private location: Location) {
     // detect os setup
     let os: string = platform.os.family;
-    if (os.indexOf('OS X') > -1) { router.navigate(['/apps/mac']); }
-    else if (os.indexOf('Window') > -1) { router.navigate(['/apps/windows']); }
+    if (os.indexOf('OS X') > -1) {
+      router.navigate(['/apps/mac']);
+      location.replaceState('/apps/mac');
+    }
+    else if (os.indexOf('Window') > -1) {
+      router.navigate(['/apps/windows']);
+      location.replaceState('/apps/mac');
+    }
     else if (os.indexOf('Fedora') > -1 ||
              os.indexOf('Red Hat') > -1 ||
              os.indexOf('CentOS') > -1 ||
@@ -19,7 +26,11 @@ export class AppsComponent {
              os.indexOf ('Xubuntu') > -1 ||
              os.indexOf('Mint')) {
       router.navigate(['/apps/linux']);
+      location.replaceState('/apps/mac');
     }
-    else { router.navigate(['/apps/windows']); }
+    else {
+      router.navigate(['/apps/windows']);
+      location.replaceState('/apps/mac');
+    }
   }
 }
