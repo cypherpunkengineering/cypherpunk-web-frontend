a5233f20f9b9488e11b21e76937bd8ef71b1a541
Edward Kim <ed@slickage.com>
2017-01-16 14:28:21 +0900

    Updated Features page with text

--- src/app/layouts/public/features/features.component.ts
+++ src/app/layouts/public/features/features.component.ts
@@ -9,6 +9,20 @@ export class FeaturesComponent implements AfterViewInit {
   title: string = 'How Cypherpunk Privacy Protects Your Online Privacy and Freedom';
   description: string = 'Learn how Cypherpunk Privacy provides unrestricted access to the internet and protects your privacy online.';
 
+  pnSwitch: boolean = false;
+  psSwitch: boolean = false;
+  esSwitch: boolean = false;
+  eppSwitch: boolean = false;
+  snSwitch: boolean = false;
+  mepSwitch: boolean = false;
+  svSwitch: boolean = false;
+
+  cpSwitch: boolean = false;
+  mfSwitch: boolean = false;
+  faSwitch: boolean = false;
+  fwSwitch: boolean = false;
+  fnSwitch: boolean = false;
+
   constructor(@Inject(DOCUMENT) private document: any) { }
 
   ngAfterViewInit(): void { this.document.title = this.title; }
