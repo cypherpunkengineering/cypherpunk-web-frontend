412088af62f68a73e9ec76a719a38858691e9dfb
J. Maurice <j@wiz.biz>
2017-01-31 23:32:01 +0900

    Restructure repo layout for multiple appengine projects (#29)

---
+++ cypherpunk.com/build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -x
+set -e
+
+## build vars
+#export NODE_VERSION=v6.9.4
+#export NVM_DIR=$HOME/.nvm
+#
+## load nvm depending on OS
+#case `uname -s` in
+#	Darwin)
+#		source "$(brew --prefix nvm)/nvm.sh" ${NODE_VERSION}
+#		;;
+#	FreeBSD|Linux)
+#		source $HOME/.nvm/nvm.sh ${NODE_VERSION}
+#		;;
+#esac
+#
+## prepare node/npm
+#nvm install ${NODE_VERSION}
+#nvm use ${NODE_VERSION}
+#
+# prepare local deps
+npm install
+
+# start build
+npm run build:prod:ngc
+node ./scripts/checkServer
+npm run serve &
+sleep 5
+
+# start scrape
+npm run scrape
+
+# deploy to appengine static
+cd appengine/
+gcloud config set project cypherpunk-engineering
+echo y | gcloud app deploy
+
+# done
+exit 0
