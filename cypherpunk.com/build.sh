88aa020d23919007953c142ba5f4305360caa0c2
Edward Kim <ed@slickage.com>
2017-02-08 22:02:47 -1000

    Versoning printout, fixing landing page copying

--- cypherpunk.com/build.sh
+++ cypherpunk.com/build.sh
@@ -22,6 +22,13 @@ nvm use ${NODE_VERSION}
 # prepare local deps
 npm install
 
+# debugging tools
+GIT_HASH="$(git describe --always --match=nosuchtagpattern --dirty=-p)"
+[ -z "${BUILD_NUMBER}" ] && BUILD_NUMBER="manually"
+BUILD_INFO="Deployed ${BUILD_NUMBER} on $(date) from revision ${GIT_HASH}"
+echo $BUILD_INFO
+sed -i '' "s/__BUILD_INFO__/${BUILD_INFO}/" src/index.ejs
+
 # start build
 npm run build:prod:ngc
 node ./scripts/checkServer
