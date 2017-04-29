6e3d55e7ca8aaa94ce60db9622d94d392653d327
Edward Kim <ed@slickage.com>
2017-04-28 16:54:34 -1000

    Updated network page

--- cypherpunk-com/src/app/layouts/public/network/network.component.ts
+++ cypherpunk-com/src/app/layouts/public/network/network.component.ts
@@ -11,8 +11,14 @@ import 'rxjs/add/observable/forkJoin';
 })
 export class NetworkComponent implements OnInit {
   regionArray = [];
+  locationArray = [];
   loading: boolean = true;
   totalServers: number = 0;
+
+  // Map Stuff
+  pi = Math.PI;
+  halfPi = this.pi / 2;
+  epsilon = Number.EPSILON;
 
   constructor(
     private router: Router,
@@ -32,6 +38,8 @@ export class NetworkComponent implements OnInit {
         // parse data
         let locations = data[0];
         let regions = data[1];
+
+        console.log(locations);
 
         // create regionsArray
         regions.regionOrder.forEach((regionId) => {
@@ -58,6 +66,16 @@ export class NetworkComponent implements OnInit {
           region.countries.sort(this.regionSort);
         });
 
+        console.log(this.regionArray);
+        this.regionArray.forEach((region) => {
+          region.countries.forEach((location) => {
+            this.locationArray.push(location);
+          });
+        });
+
+        this.translateLocations(this.locationArray);
+        console.log(this.locationArray);
+
         this.loading = false;
       },
       (error: any) => {
@@ -82,5 +100,42 @@ export class NetworkComponent implements OnInit {
     else if (a.country === b.country && a.name > b.name) { value = 1; }
     else if (a.country === b.country && a.name < b.name) { value = -1; }
     return value;
+  }
+
+  vanDerGrinten3Raw(lambda, phi) {
+    if (Math.abs(phi) < this.epsilon) { return [lambda, 0]; }
+
+    let sinTheta = phi / this.halfPi;
+    let theta = Math.asin(sinTheta);
+
+    if (Math.abs(lambda) < this.epsilon || Math.abs(Math.abs(phi) - this.halfPi) < this.epsilon) {
+      return [0, this.pi * Math.tan(theta / 2)];
+    }
+
+    let A = (this.pi / lambda - lambda / this.pi) / 2;
+    let y1 = sinTheta / (1 + Math.cos(theta));
+
+    return [
+      this.pi * (Math.sign(lambda) * Math.sqrt(A * A + 1 - y1 * y1) - A),
+      this.pi * y1
+    ];
+  }
+
+  transformToXY(lat, long) {
+    let coords = this.vanDerGrinten3Raw((long - 11) * this.pi / 180, lat * this.pi / 180);
+    coords[0] = (coords[0] * 150 + (920 / 2)) * (1000 / 920);
+    coords[1] = (coords[1] * 150 + (500 / 2 + 500 * 0.15)) * (1000 / 920);
+    return coords;
+  }
+
+  translateLocations(servers) {
+    servers.forEach((server) => {
+      if (!server.lat || !server.lon) { return; }
+      let [ x, y ] = this.transformToXY(server.lat, server.lon);
+      server.mapX = x;
+      server.mapY = y;
+      server.scale = server.scale || 1;
+    });
+    return servers;
   }
 }
