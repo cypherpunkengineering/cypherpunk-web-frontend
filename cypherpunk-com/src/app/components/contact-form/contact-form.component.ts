070fc5a5ce5df4ec987a0dc644579188ff0b5eb1
J. Maurice <j@wiz.biz>
2017-05-17 18:55:58 +0900

    Fix compilation error due to typo

--- cypherpunk-com/src/app/components/contact-form/contact-form.component.ts
+++ cypherpunk-com/src/app/components/contact-form/contact-form.component.ts
@@ -18,9 +18,9 @@ export class ContactFromComponent {
   groups = [
     { value: '32425127', title: 'Customer Support'},
     { value: '33432807', title: 'Billing Question'},
-    { value: '42217047', title: 'Leave Feedback'}
+    { value: '42217047', title: 'Leave Feedback'},
     { value: '42228188', title: 'Business Development'},
-    { value: '42250028', title: 'Press / Media Inquiries'},
+    { value: '42250028', title: 'Press / Media Inquiries'}
   ];
 
   nameTouched: boolean;
