b77b4994c856b99d4488c60dc45676871da28256
Edward Kim <ed@slickage.com>
2017-05-17 21:08:45 -1000

    Generate ToC on support articles

--- cypherpunk-com/src/app/layouts/public/support/articles/support-article.component.ts
+++ cypherpunk-com/src/app/layouts/public/support/articles/support-article.component.ts
@@ -1,13 +1,18 @@
+import { Location } from '@angular/common';
+import { isBrowser } from 'angular2-universal';
 import { DOCUMENT } from '@angular/platform-browser';
 import { Router, ActivatedRoute } from '@angular/router';
 import { BackendService } from '../../../../services/backend.service';
-import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
+import { Component, Inject, OnInit, OnDestroy, AfterViewChecked } from '@angular/core';
 
 @Component({
   templateUrl: './support-article.component.html',
   styleUrls: ['./support-article.component.css']
 })
-export class SupportArticleComponent implements OnInit, OnDestroy {
+export class SupportArticleComponent implements OnInit, AfterViewChecked, OnDestroy {
+  contentLoaded: boolean = false;
+  pageLinks = [];
+  baseRoute: String;
   post = {
     id: '',
     title: '',
@@ -19,10 +24,13 @@ export class SupportArticleComponent implements OnInit, OnDestroy {
 
   constructor(
     private router: Router,
+    private location: Location,
     private route: ActivatedRoute,
     private backend: BackendService,
     @Inject(DOCUMENT) private document: any
-  ) { }
+  ) {
+    this.baseRoute = location.path();
+  }
 
   ngOnInit() {
     let id = this.route.snapshot.params['id'];
@@ -36,6 +44,34 @@ export class SupportArticleComponent implements OnInit, OnDestroy {
       .catch((err) => {
         this.router.navigate(['/support']);
       });
+    }
+  }
+
+  ngAfterViewChecked() {
+    if (this.contentLoaded) { return; }
+    if (isBrowser) {
+      // get all h3 tags
+      let elements = document.querySelectorAll('h3');
+      if (elements.length) { this.contentLoaded = true; }
+      for (let i = 0; i < elements.length; i++) {
+        // create id hash
+        let title = elements[i].textContent;
+        let text = title.toLowerCase();
+        text = text.trim();
+        text = text.replace(/ /g, '-');
+        let hash = text + '-' + i;
+
+        // bind id to child element
+        let span = document.createElement('span');
+        span.id = hash;
+        span.className = 'features-anchor-target';
+        elements[i].appendChild(span);
+
+        // save hash for displaying later
+        setTimeout(() => {
+          this.pageLinks.push({ 'title': title, hash: hash});
+        });
+      }
     }
   }
 
