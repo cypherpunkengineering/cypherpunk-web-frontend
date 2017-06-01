1603847783c0a46d77999c4c0d9812b8ba0bc777
J. Maurice <j@wiz.biz>
2017-06-01 13:38:42 +0900

    Update desktop app download links with build 1246

--- cypherpunk-com/src/app/layouts/public/apps/platform-builds.ts
+++ cypherpunk-com/src/app/layouts/public/apps/platform-builds.ts
@@ -3,21 +3,21 @@ export class PlatformBuilds {
     name: 'MacOS',
     appLink: '/apps/macos',
     versions: '',
-    link: 'https://download.cypherpunk.com/release/macos/cypherpunk-privacy-macos-0.6.1-beta-00452.pkg'
+    link: 'https://download.cypherpunk.com/release/macos/cypherpunk-privacy-macos-0.7.0-beta-01246.pkg'
   };
 
   static macos = {
     name: 'MacOS',
     appLink: '/apps/macos',
     versions: '',
-    link: 'https://download.cypherpunk.com/release/macos/cypherpunk-privacy-macos-0.6.1-beta-00452.pkg'
+    link: 'https://download.cypherpunk.com/release/macos/cypherpunk-privacy-macos-0.7.0-beta-01246.pkg'
   };
 
   static windows = {
     name: 'Windows',
     appLink: '/apps/windows',
     versions: '7 64-bit and later',
-    link: 'https://download.cypherpunk.com/release/windows/cypherpunk-privacy-windows-0.6.1-beta-00416.exe'
+    link: 'https://download.cypherpunk.com/release/windows/cypherpunk-privacy-windows-0.7.0-beta-01246.exe'
   };
 
   static linux = {
@@ -84,7 +84,7 @@ export class PlatformBuilds {
       name: 'Debian/Ubuntu/Mint',
       os: 'Debian/Ubuntu/Mint',
       version: '64-bit',
-      link: 'https://download.cypherpunk.com/release/debian/cypherpunk-privacy-linux-x64_0.6.1-beta-00131.deb'
+      link: 'https://download.cypherpunk.com/release/debian/cypherpunk-privacy-linux-0.7.0-beta-01246.deb'
     }
   ];
 
