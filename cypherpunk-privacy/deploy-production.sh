a692c5105410dcc013320cc2b6cf1c946575dd99
J. Maurice <j@wiz.biz>
2017-06-15 19:32:56 +0900

    Create separate scripts to deploy to staging or production

---
+++ cypherpunk-privacy/deploy-production.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+umask 022
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+cp ./appengine-web-production.xml ./src/main/webapp/WEB-INF/appengine-web.xml
+mvn appengine:update
+rm -f ./src/main/webapp/WEB-INF/appengine-web.xml
+
+exit 0
