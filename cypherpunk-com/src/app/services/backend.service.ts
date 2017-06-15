534571c7871c84e9f6913e912f124de75e365bc8
Edward Kim <ed@slickage.com>
2017-06-14 16:15:11 -1000

    Remove trailing slash from /pricing/plans api endpoint

--- cypherpunk-com/src/app/services/backend.service.ts
+++ cypherpunk-com/src/app/services/backend.service.ts
@@ -59,7 +59,7 @@ export class BackendService {
   // User account apis
 
   pricingPlans(referralCode, options): Promise<any> {
-    let url = this.globals.API_URL + '/pricing/plans/' + referralCode;
+    let url = this.globals.API_URL + '/pricing/plans' + referralCode;
     return this.http.get(url, options).toPromise()
     .then(this.parseJson)
     .catch(this.catchFunction);
