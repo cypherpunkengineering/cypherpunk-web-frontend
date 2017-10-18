9121f87a4a4ac64d0ae963201b4ba9ce50edd7df
Kim Nilsson <kim@cypherpunk.com>
2017-10-19 06:12:08 +0900

    Rework account-creation-form component

--- cypherpunk-com/src/app/components/forms/account-creation-form/account-creation-form.component.ts
+++ cypherpunk-com/src/app/components/forms/account-creation-form/account-creation-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, NgZone } from '@angular/core';
 import { BackendService } from '../../../services/backend.service';
-import { FormBuilder, FormGroup, Validators, AbstractControl, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ValidationErrors, AbstractControl, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-account-creation-form',
@@ -14,6 +15,8 @@ export class AccountCreationFormComponent implements OnInit {
   email: AbstractControl;
   password: AbstractControl;
 
+  emailCheckResults: { [email: string]: boolean; };
+
   constructor(
     private zone: NgZone,
     private fb: FormBuilder,
@@ -22,9 +25,8 @@ export class AccountCreationFormComponent implements OnInit {
 
   ngOnInit() {
     this.accountForm = this.fb.group({
-      email: [
-        this.accountFormData.email,
-        Validators.compose([ Validators.required, EmailValidator.validate])
+      email: [ this.accountFormData.email,
+        Validators.compose([Validators.required, EmailValidator.validate])
       ],
       password: [ this.accountFormData.password,
         Validators.compose([Validators.required, PasswordValidator.validate])
@@ -34,6 +36,8 @@ export class AccountCreationFormComponent implements OnInit {
 
     this.email = this.accountForm.controls['email'];
     this.password = this.accountForm.controls['password'];
+
+    this.emailCheckResults = {};
 
     this.accountForm.valueChanges.subscribe((form: any) => {
       if (this.accountForm.disabled) {
@@ -47,6 +51,12 @@ export class AccountCreationFormComponent implements OnInit {
     });
   }
 
+  enableInputs() {
+    this.accountForm.get('email').enable();
+    this.accountForm.get('password').enable();
+    this.accountFormData.validation.disabled = false;
+  }
+
   disableInputs() {
     this.accountForm.get('email').disable();
     this.accountForm.get('password').disable();
@@ -55,27 +65,76 @@ export class AccountCreationFormComponent implements OnInit {
 
   validateEmail(): void {
     // check if email is already taken
-    let body = { email: this.accountFormData.email };
-    this.backend.identifyEmail(body, {})
+    let email = this.accountFormData.email;
+    if (this.email.invalid || this.emailCheckResults.hasOwnProperty(email)) { return; }
+    this.backend.identifyEmail({ email }, {})
     .then(() => {
       this.zone.run(() => {
+        this.emailCheckResults[email] = false;
         this.accountFormData.validation.email = false;
         this.accountFormData.validation.warning = true;
       });
     })
     .catch((data) => {
       this.zone.run(() => {
-        if (data.status === 401) { this.accountFormData.validation.email = true; }
-        else { this.accountFormData.validation.email = false; }
+        if (data.status === 401) {
+          this.emailCheckResults[email] = true;
+          this.accountFormData.validation.email = true;
+        } else { this.accountFormData.validation.email = false; }
       });
     });
   }
+
+  measurePasswordStrength(password) : string {
+    function getStats(password) {
+      let uppercase = 0, lowercase = 0, digits = 0, others = 0;
+      let uniqueDict = {}, unique = 0;
+      for (let i = 0; i < password.length; i++) {
+        const ch = password[i];
+        if (uniqueDict.hasOwnProperty(ch)) continue;
+        if (ch >= 'A' && ch <= 'Z') uppercase++;
+        else if (ch >= 'a' && ch <= 'z') lowercase++;
+        else if (ch >= '0' && ch <= '9') digits++;
+        else if (ch > ' ' && ch <= '\x7E') others++;
+        uniqueDict[ch] = true;
+        unique++;
+      }
+      let classes = +!!uppercase + +!!lowercase + +!!digits + +!!others;
+      return { uppercase, lowercase, digits, others, classes, unique };
+    }
+    function getScoreRequirements(stats) {
+      switch (stats.classes) {
+        case 4:
+          return [6, 8, 12];
+        case 3:
+          if (stats.others) return [6, 10, 14];
+          return [6, 12, 16];
+        case 2:
+          if (stats.digits || stats.others) return [10, 16, 24];
+          return [12, 18];
+        case 1:
+          if (stats.others) return [10];
+          return [14];
+      }
+      return [];
+    }
+    function getScore(limits, length) {
+      for (let i = limits.length; i > 0; i--) {
+        if (length >= limits[i - 1]) return i;
+      }
+      return 0;
+    }
+
+    const stats = getStats(password);
+    const limits = getScoreRequirements(stats);
+    const length = Math.min(stats.unique * 2, password.length);
+    const score = getScore(limits, length);
+    return "strength-" + score;
+  }
 }
 
-interface ValidationResult { [key: string]: boolean; }
-
 class EmailValidator {
-  static validate(control: FormControl): ValidationResult {
+  static validate(control: FormControl): ValidationErrors {
     // test that email is in right format
     if (!/^\S+@\S+$/.test(control.value)) { return { 'invalidEmailFormat': true }; }
     else { return null; }
@@ -83,10 +142,10 @@ class EmailValidator {
 }
 
 class PasswordValidator {
-  static validate(control: FormControl): ValidationResult {
-    // test that email is in right format
-    if (!/^[\x21-\x7E]*$/.test(control.value)) { return { 'invalidPasswordFormat': true }; }
-    else if (control.value.length < 6) { return { 'invalidPasswordFormat': true }; }
+  static validate(control: FormControl): ValidationErrors {
+    // test that password is in right format
+    if (!/^[\x20-\x7E]*$/.test(control.value)) { return { 'invalidPasswordFormat': true }; }
+    else if (control.value.length < 6) { return { 'passwordTooShort': true }; }
     else { return null; }
   }
 }
